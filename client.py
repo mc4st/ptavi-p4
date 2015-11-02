722325621ad327812694a0ea1d1568272c5b0864
mc4st <C4st3j0n>
2015-11-02 17:33:07 +0100

    8

--- client.py
+++ client.py
@@ -6,19 +6,17 @@ Programa cliente que abre un socket a un servidor
 
 import socket
 import sys
-# Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-IP = sys.argv[1]
-PORT = int(sys.argv[2])
-
-metodo = sys.argv[3]
-if metodo == 'register':
-    ADDRESS = sys.argv[4]
+if len(sys.argv) == 6:
+    SERVER = 'localhost'
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    EXPIRES = int(sys.argv[5])
+    metodo = sys.argv[3]
+    if  metodo == 'register':
+        ADDRESS = sys.argv[4]
 else:
-    sys.exit("python3 client.py ip puerto register luke@polismassa.com")
-
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,7 +24,7 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 METODO = "REGISTER"
-PETICION = METODO + " " + ADDRESS + " " + "SIP/2.0" "\r\n" "\r\n"
+PETICION = METODO + " " + ADDRESS + " " + "SIP/2.0" + "\r\n" + "Expires: " + str(EXPIRES) + "\r\n" + "\r\n"
 print("Enviando: " + PETICION)
 my_socket.send(bytes(PETICION, 'utf-8'))
 data = my_socket.recv(1024) #Tamaño del Buffer 1024
