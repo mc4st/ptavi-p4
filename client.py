9044778f342e02b0a1a379fba7353cae24e63745
mc4st <C4st3j0n>
2015-11-02 20:19:44 +0100

    Final

--- client.py
+++ client.py
@@ -18,20 +18,19 @@ if len(sys.argv) == 6:
 else:
     sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 METODO = "REGISTER"
-PETICION = METODO + " sip:" + ADDRESS + " " + "SIP/2.0" + "\r\n" + "Expires: " + str(EXPIRES) + "\r\n" + "\r\n"
+PETICION = METODO + " " + ADDRESS + " " + "SIP/2.0" + "\r\n" + "Expires: " + str(EXPIRES) + "\r\n" + "\r\n"
 print("Enviando: " + PETICION)
 my_socket.send(bytes(PETICION, 'utf-8'))
 data = my_socket.recv(1024)
+""" Tamaño del Buffer 1024 """
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
 
-# Cerramos todo
 my_socket.close()
 print("Fin.")
